948e15d6bb908234a35e68e8dd7fb5fbdf58b87b
Ricardo Franco <ricardofranco2509@gmail.com>
2023-08-22 17:45:58 -0300

    criando o navbar

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Navbar from './components/Navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div>
-      Hello world
-    </div>
+    <Navbar />
   </React.StrictMode>
 );
