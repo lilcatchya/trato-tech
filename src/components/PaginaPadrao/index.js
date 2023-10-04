6f08988922f14d00eaa06b91924da39322d5254a
Ricardo Franco <ricardofranco2509@gmail.com>
2023-10-04 10:39:01 -0300

    git add . git commit -m "

--- src/components/PaginaPadrao/index.js
+++ src/components/PaginaPadrao/index.js
@@ -1,12 +1,16 @@
 import { Outlet } from "react-router-dom"
 import styles from "./PaginaPadrao.module.scss"
+import Footer from "../Footer"
+import Navbar from "../Navbar"
 
 export default function PaginaPadrao() {
   return (
     <div className={styles.container}>
+      <Navbar />
       <div className={styles['container-outlet']}>
         <Outlet />
       </div>
+      <Footer />
     </div>
   )
 }
